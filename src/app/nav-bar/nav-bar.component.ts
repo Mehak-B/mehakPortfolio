b09bfe72c05a4ddcfd381531b970b1115f624d2b
Mehak-B <mehakbhardwaj2013@gmail.com>
2025-04-07 22:52:29 +0530

    added intoduction component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IntroductionComponent } from "../introduction/introduction.component";
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [IntroductionComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
   navBarItems = ["Home", "About", "Projects", "Contact"]
+
+  // count = signal(0);
+ 
+  ngOnInit(){ 
+  // this.count.set(6)
+  // console.log('The count is: ' + this.count());
+  // console.log(this.countSet())
+  // }
+
+  // countSet(){
+  //  this.count.set(10)
+  //  return this.count()
+
+  }
 
 }
