ce272daddb472939500db08850e3111b8564e649
Mehak-B <mehakbhardwaj2013@gmail.com>
2025-05-01 13:21:44 +0530

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
-export const routes: Routes = [
-    {path:"", component:NavBarComponent}
-];
